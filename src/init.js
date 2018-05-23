2bd4dbef9fd6856ac4023f3c6d1d3bcc7f889c13
Pavel Jurasek <pavel.jurasek@joincoup.com>
2018-05-23 16:03:27 +0200

    fix async for segment analytics

--- src/init.js
+++ src/init.js
@@ -6,98 +6,56 @@ export default function init (config, callback) {
     return
   }
 
-  // Create a queue, but don't obliterate an existing one!
-  var analytics = window.analytics = window.analytics || []
+  (function() {
+    var
+        analytics = window.analytics = window.analytics || [];
+    if (!analytics.initialize)
+        if (analytics.invoked) window.console && console.error && console.error("Segment snippet included twice.");
+        else {
+            analytics.invoked = !0;
+            analytics.methods = ["trackSubmit", "trackClick", "trackLink", "trackForm", "pageview", "identify", "reset", "group", "track", "ready", "alias", "page", "once", "off", "on"];
+            analytics.factory = function(t) {
+                return function() {
+                    var e = Array.prototype.slice.call(arguments);
+                    e.unshift(t);
+                    analytics.push(e);
+                    return
+                    analytics
+                }
+            };
+            for (var t = 0; t < analytics.methods.length; t++) {
+                var
+                    e = analytics.methods[t];
+                analytics[e] = analytics.factory(e)
+            }
+            analytics.load = function(t) {
+                var
+                    e = document.createElement("script");
+                e.type = "text/javascript";
+                e.async = !0;
+                e.src = ("https:" === document.location.protocol ? "https://" : "http://") + "cdn.segment.com/analytics.js/v1/" + t + "/analytics.min.js";
+                var
+                    n = document.getElementsByTagName("script")[0];
+                n.parentNode.insertBefore(e, n)
+            };
+            analytics.SNIPPET_VERSION = "3.1.0";
+            analytics.load(config.id);
+            // Make sure to remove any calls to `analytics.page()`!
+        }
+  })();
 
-  // If the real analytics.js is already on the page return.
-  if (analytics.initialize) return
-
-  // If the snippet was invoked already show an error.
-  if (analytics.invoked) {
-    if (window.console && console.error) {
-      console.error('Segment snippet included twice.')
+  const poll = setInterval(function () {
+    if (!window.analytics) {
+      return
     }
-    return
-  }
 
-  // Invoked flag, to make sure the snippet
-  // is never invoked twice.
-  analytics.invoked = true
+    clearInterval(poll)
 
-  // A list of the methods in Analytics.js to stub.
-  analytics.methods = [
-    'trackSubmit',
-    'trackClick',
-    'trackLink',
-    'trackForm',
-    'pageview',
-    'identify',
-    'reset',
-    'group',
-    'track',
-    'ready',
-    'alias',
-    'debug',
-    'page',
-    'once',
-    'off',
-    'on'
-  ]
-
-  // Define a factory to create stubs. These are placeholders
-  // for methods in Analytics.js so that you never have to wait
-  // for it to load to actually record data. The `method` is
-  // stored as the first argument, so we can replay the data.
-  analytics.factory = function (method) {
-    return function () {
-      var args = Array.prototype.slice.call(arguments)
-      if (config.debug === true) {
-        if (window.console && console.log) {
-          console.log(`[Segment Analytics Debug]: ${method} method called with ${args.length} args`)
-        }
-      } else {
-        args.unshift(method)
-        analytics.push(args)
-        return analytics
-      }
-    }
-  }
-
-  // Add a version to keep track of what's in the wild.
-  analytics.SNIPPET_VERSION = '4.0.0';
-
-  // For each of our methods, generate a queueing stub.
-  for (let key of analytics.methods) {
-    analytics[key] = analytics.factory(key);
-  }
-
-  if (config.debug === false) {
-    const source = `https://cdn.segment.com/analytics.js/v1/${config.id}/analytics.min.js`
-    loadScript(source, function (error, script) {
-      if (error) {
-        console.warn('Ops! Is not possible to load Segment Analytics script')
-        return
-      }
-
-      const poll = setInterval(function () {
-        if (!window.analytics) {
-          return
-        }
-
-        clearInterval(poll)
-
-        // the callback is fired when window.analytics is available and before any other hit is sent
-        if (callback && typeof callback === 'function') {
-          callback()
-        }
-      }, 10)
-    })
-  } else {
-    // Still run the callback in debug mode.
+    // the callback is fired when window.analytics is available and before any other hit is sent
     if (callback && typeof callback === 'function') {
       callback()
     }
-  }
+  }, 10);
 
   return window.analytics
 }
